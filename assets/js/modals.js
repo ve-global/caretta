6f04c083939e3c720a45ffa099255f27781ccca2
VE\jKottis <john.kottis@veinteractive.com>
2016-10-17 17:06:37 +0100

    eslint modals.js

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -3,71 +3,73 @@
  *  Date: 13/10/2016
  *  Caretta Framework
  */
+'use strict';
 
-var Caretta = Caretta || {};
+var Caretta;
 
-Caretta.Modals = (function() {
+Caretta = Caretta || {};
 
-	/**
-	* Open modal
-	* e {object} 		- event
-	*/
-    let TriggerModal = (e) => {
-        e.preventDefault();
-        let modalId = e.target.getAttribute('data-modal'),
-        	overlay = document.createElement('DIV');
-        
-        overlay.id = 'body-overlay';
-        document.body.appendChild(overlay);
-        document.getElementById(modalId).classList.add('open');
-    },
+Caretta.Modals = (function () {
 
     /**
-	* Close current modal
-	* e {object} 		- event
-	*/
-    triggerCloseModal = (e) => {
-        e.preventDefault();
-        let modal = findAncestor(e.target, 'modal'),
-        	overlay = document.getElementById('body-overlay');
-        
-        document.body.removeChild(overlay);
-        modal.classList.remove('open');
-    },
+     *** HELPER FUNCTION ***
+     * Get the parent with specific class
+     * el {object}      - child element
+     * cls {string}     - parent class
+     */
+    let findAncestor = (el, cls) => {
+            while ((el = el.parentElement) && !el.classList.contains(cls));
+            return el;
+        },
 
-    /**
-	* Initialize click event for modals triggering
-	*/
-    initModals = () => {
-        let modals = document.querySelectorAll('[data-toggle="modal"]');
-        
-        for (let i = 0; i < modals.length; i++){
-          	modals[i].addEventListener('click', triggerModal);
-        }
-    },
+        /**
+         * Open modal
+         * e {object}       - event
+         */
+        triggerModal = (e) => {
+            e.preventDefault();
+            let modalId = e.target.getAttribute('data-modal'),
+                overlay = document.createElement('DIV');
 
-    /**
-	* Initialize click event for closing modals
-	*/
-    initCloseModals = () => {
-    	let closeModals = document.querySelectorAll('[data-dismiss="modal"]');
-        
-        for (let i = 0; i < closeModals.length; i++){
-          	closeModals[i].addEventListener('click', triggerCloseModal);
-        }
-    },
+            overlay.id = 'body-overlay';
+            document.body.appendChild(overlay);
+            document.getElementById(modalId).classList.add('open');
+        },
 
-    /**
-	*** HELPER FUNCTION ***
-	* Get the parent with specific class
-	* el {object} 		- child element
-	* cls {string}		- parent class
-	*/
-	findAncestor = (el, cls) => {
-	    while ((el = el.parentElement) && !el.classList.contains(cls));
-	    return el;
-	};
+        /**
+         * Close current modal
+         * e {object}       - event
+         */
+        triggerCloseModal = (e) => {
+            e.preventDefault();
+            let modal = findAncestor(e.target, 'modal'),
+                overlay = document.getElementById('body-overlay');
 
+            document.body.removeChild(overlay);
+            modal.classList.remove('open');
+        },
+
+        /**
+         * Initialize click event for modals triggering
+         */
+        initModals = () => {
+            let modals = document.querySelectorAll('[data-toggle="modal"]');
+
+            for (let i = 0; i < modals.length; i++) {
+                modals[i].addEventListener('click', triggerModal);
+            }
+        },
+
+        /**
+         * Initialize click event for closing modals
+         */
+        initCloseModals = () => {
+            let closeModals = document.querySelectorAll('[data-dismiss="modal"]');
+
+            for (let i = 0; i < closeModals.length; i++) {
+                closeModals[i].addEventListener('click', triggerCloseModal);
+            }
+        };
 
     return {
         initModals: initModals,
